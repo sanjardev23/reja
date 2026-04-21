0b475a250aa18857b5128ed769edfe391ab5dd7e
sanjardev23 <mksanjar04@gmail.com>
2026-04-22 00:14:04 +0900

    feat: develop author router ejs page

--- server.js
+++ server.js
@@ -2,8 +2,19 @@
 console.log('Web Serverni boshlash');
 
 const express = require("express");
+const res = require("express/lib/response")
 const app = express();
 const http = require("http")
+const fs = require("fs");
+
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+    if(err) {
+        console.log("ERROR: ", err);
+    } else {
+        user = JSON.parse(data)
+    }
+});
 
 // 1: Kirish code
 app.use(express.static('public'));
@@ -22,6 +33,10 @@ app.set("view engine", "ejs");
 app.post("/create-item", (req, res) => {
     console.log(req.body);
     res.json({ test: "success" });
+});
+
+app.get("/author", function(req, res) {
+    res.render("author", {user: user});
 });
 
 app.get("/", function(req, res) {
