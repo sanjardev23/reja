a0f8fbe6d549b6daa1dfdfaa9375f39e551a53ba
sanjardev23 <mksanjar04@gmail.com>
2026-04-24 04:40:45 +0900

    feat: train callback hamda asyn functions

--- train.js
+++ train.js
@@ -22,38 +22,41 @@ const list = [
     'Spend time for yourself', // 60
 ];
 
-function giveAdvice(a, callback) {
-    if(typeof a !== 'number') callback('Please insert a number!', null)
-    else if(a <= 20) setTimeout(() => callback(null, list[0]), 5000);
-    else if(a > 20 && a <= 30) setTimeout(() => callback(null, list[1]), 3000);
-    else if(a > 30 && a <= 40) setTimeout(() => callback(null, list[2]), 3000);
-    else if(a > 40 && a <= 50) setTimeout(() => callback(null, list[3]), 3000);
-    else if(a > 50 && a <= 60) setTimeout(() => callback(null, list[4]), 3000);
-    else setTimeout(() => callback(null, list[5]), 5000);
-}
-
-
 // function giveAdvice(a, callback) {
 //     if(typeof a !== 'number') callback('Please insert a number!', null)
-//     else if(a <= 20) callback(null, list[0]);
-//     else if(a > 20 && a <= 30) callback(null, list[1]);
-//     else if(a > 30 && a <= 40) callback(null, list[2]);
-//     else if(a > 40 && a <= 50) callback(null, list[3]);
-//     else if(a > 50 && a <= 60) callback(null, list[4]);
-//     else {
-//         setTimeout(() => {
-//             callback(null, list[5])
-//         }, 5000);
-//     }
+//     else if(a <= 20) setTimeout(() => callback(null, list[0]), 5000);
+//     else if(a > 20 && a <= 30) setTimeout(() => callback(null, list[1]), 3000);
+//     else if(a > 30 && a <= 40) setTimeout(() => callback(null, list[2]), 3000);
+//     else if(a > 40 && a <= 50) setTimeout(() => callback(null, list[3]), 3000);
+//     else if(a > 50 && a <= 60) setTimeout(() => callback(null, list[4]), 3000);
+//     else setTimeout(() => callback(null, list[5]), 5000);
 // }
 
 
 
+// CALLBACK function
+function giveAdvice(a, callback) {
+    if(typeof a !== 'number') callback('Please insert a number!', null)
+    else if(a <= 20) callback(null, list[0]);
+    else if(a > 20 && a <= 30) callback(null, list[1]);
+    else if(a > 30 && a <= 40) callback(null, list[2]);
+    else if(a > 40 && a <= 50) callback(null, list[3]);
+    else if(a > 50 && a <= 60) callback(null, list[4]);
+    else {
+        setInterval(function () {
+            callback(null, list[5])        // ==> async ni ichida core module functionlar ishlamaydi
+        }, 1000);
+    }
+}
+
+
+
 console.log('passed here 0')
-giveAdvice(15, (err, data) => {
+giveAdvice(70, (err, data) => {
     if(err) console.log('ERROR:', err);
     else { 
-        console.log(`Advice for your age: ${data}`);
+        // console.log(`Advice for your age: ${data}`);
+        console.log(data)
     }
 });
 console.log('passed here 1')
@@ -61,9 +64,64 @@ console.log('passed here 1')
 
 
 
-// Task
-// C-Task
-// B-Task
-// A-TASK
 
 
+
+
+// Asynchronouse functions
+// Promise functions
+// Callback vs Asynchronouse vs Promise
+
+
+// ASYNC function (def)
+// async function giveAdvice(a) {
+//     if(typeof a !== 'number') throw new Error ('Please insert a number!', null)
+//     else if(a <= 20) return list[0];
+//     else if(a > 20 && a <= 30) return list[1];
+//     else if(a > 30 && a <= 40) return list[2];
+//     else if(a > 40 && a <= 50) return list[3];
+//     else if(a > 50 && a <= 60) return list[4];
+//     else {
+//         return new Promise((resolve, reject) => {
+//             setTimeout(() => {
+//                 resolve(list[5]);
+//             }, 5000)
+//         });
+
+
+// resolve bu return ga teng
+// promise ichida code functionlar(setTimeOut, setInterval) yaxshi ishlaydi, asyncda ishlamaydi
+
+
+//         // setTimeout(() => {
+//         //     return list[5]       ==> async ni ichida core module functionlar ishlamaydi
+//         // }, 5000);               
+//     }
+// }
+
+
+// call via then/catch
+// console.log('passed here 0')
+// giveAdvice(31)
+//     .then((data) => {
+//         console.log(`Advice for your age: ${data}`);
+//     })
+//     .catch((err) => {
+//         console.log('ERROR:', err);
+//     })
+// console.log('passed here 1')
+
+// sync function pass bolgandan kn node js async bn ishlaydi
+
+
+
+// call via async/await
+// async function run() {
+//     let answer = await giveAdvice(25);
+//     console.log(answer);
+//     answer = await giveAdvice(65);    // await bu yerda value olmaguncha keyingi stepga otmaydi
+//     console.log(answer);
+//     answer = await giveAdvice(41);
+//     console.log(answer);
+// }
+// run();
