a896b35d1b88b45f57508345bef90c4f8e3d0ecb
sanjardev23 <mksanjar04@gmail.com>
2026-04-27 17:12:20 +0900

    feat: CRUD operations

--- app.js
+++ app.js
@@ -24,12 +24,33 @@ app.set("view engine", "ejs");
 
 // 4: Routing code
 app.post("/create-item", (req, res) => {
-    console.log(req.body);
-    res.json({ test: "success" });
+    console.log('user intered /create-item');
+    const new_reja = req.body.reja;
+    db.collection("plans").insertOne({ reja: new_reja }, (err, data) => {
+        if(err) {
+            console.log(err);
+            res.end("something went wrong");
+        } else {
+            res.end("successfully added")
+        }
+    })
+
+    
+    // res.redirect("/"); // 🔥 page reload → input clears automatically
 });
 
 app.get("/", function(req, res) {
-    res.render("reja");
+    // res.render("reja");
+    console.log('user intered /');
+    db.collection("plans").find().toArray((err, data) => {
+        if(err) {
+            console.log(err);
+            res.end("Something went wrong");
+        } else {
+            // console.log(data);
+            res.render("reja", { items: data })
+        }
+    })
 });
 
 
