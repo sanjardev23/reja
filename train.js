d42799e4e6a47133549e5692c77042c77e88e3b7
sanjardev23 <mksanjar04@gmail.com>
2026-04-27 09:37:37 +0900

    fix: refactoring

--- train.js
+++ train.js
@@ -12,6 +12,20 @@
 // Request keldi → DB ga so'rov yubordi → kutmasdan keyingi requestni qabul qiladi → DB javob berganda qaytib keladi (callback/async)
 
 
+
+/* Asynchronous function > Callback | Async | Promise 
+
+    DEFINITION                  CALL
+
+    callback          >       callback
+    async/await       >       then/catch || async/await
+    promise           >       then/catch || async/await
+ 
+*/
+
+
+
+
 console.log('Jack Ma`s advice for young people')
 const list = [
     'Learn as much as you can', // 0-20
@@ -74,42 +88,46 @@ function giveAdvice(a, callback) {
 
 
 // ASYNC function (def)
-// async function giveAdvice(a) {
-//     if(typeof a !== 'number') throw new Error ('Please insert a number!', null)
-//     else if(a <= 20) return list[0];
-//     else if(a > 20 && a <= 30) return list[1];
-//     else if(a > 30 && a <= 40) return list[2];
-//     else if(a > 40 && a <= 50) return list[3];
-//     else if(a > 50 && a <= 60) return list[4];
-//     else {
-//         return new Promise((resolve, reject) => {
-//             setTimeout(() => {
-//                 resolve(list[5]);
-//             }, 5000)
-//         });
+async function giveAdvice(a) {
+    if(typeof a !== 'number') throw new Error ('Please insert a number!', null)
+    else if(a <= 20) return list[0];
+    else if(a > 20 && a <= 30) return list[1];
+    else if(a > 30 && a <= 40) return list[2];
+    else if(a > 40 && a <= 50) return list[3];
+    else if(a > 50 && a <= 60) return list[4];
+    else {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                resolve(list[5]);
+            }, 5000)
+        });
 
 
-// resolve bu return ga teng
-// promise ichida code functionlar(setTimeOut, setInterval) yaxshi ishlaydi, asyncda ishlamaydi
+        
+        
+        // setTimeout(() => {
+            //     return list[5]       ==> async ni ichida core module functionlar ishlamaydi
+            // }, 5000);               
+        }
+    }
 
 
-//         // setTimeout(() => {
-//         //     return list[5]       ==> async ni ichida core module functionlar ishlamaydi
-//         // }, 5000);               
-//     }
-// }
+    // resolve bu return ga teng
+    // promise ichida code functionlar(setTimeOut, setInterval) yaxshi ishlaydi, asyncda ishlamaydi
+    
+
 
 
 // call via then/catch
-// console.log('passed here 0')
-// giveAdvice(31)
-//     .then((data) => {
-//         console.log(`Advice for your age: ${data}`);
-//     })
-//     .catch((err) => {
-//         console.log('ERROR:', err);
-//     })
-// console.log('passed here 1')
+console.log('passed here 0')
+giveAdvice(31)
+    .then((data) => {
+        console.log(`Advice for your age: ${data}`);
+    })
+    .catch((err) => {
+        console.log('ERROR:', err);
+    })
+console.log('passed here 1')
 
 // sync function pass bolgandan kn node js async bn ishlaydi
 
