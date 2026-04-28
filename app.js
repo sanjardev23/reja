3596189516d164c390436d3aa3114aeeb3369172
sanjardev23 <mksanjar04@gmail.com>
2026-04-28 15:58:27 +0900

    feat: FrontEnd javascriptni qurish

--- app.js
+++ app.js
@@ -27,12 +27,8 @@ app.post("/create-item", (req, res) => {
     console.log('user intered /create-item');
     const new_reja = req.body.reja;
     db.collection("plans").insertOne({ reja: new_reja }, (err, data) => {
-        if(err) {
-            console.log(err);
-            res.end("something went wrong");
-        } else {
-            res.end("successfully added")
-        }
+        // console.log(data.ops)   // user inputni consolega qaytaradi
+        res.json(data.ops[0]);
     })
 
     
